3006b36a0028d3a6a9eb7d2a74bcd05830fa2084
Pratikesh-kumar <pratikeshkumar96@gmail.com>
2023-07-29 16:01:30 +0530

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  console.log(req)
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello, World!</h1>');
+  res.end('<h1>My name is Pratikesh</h1>');
 });
 
 const port = 4000;
